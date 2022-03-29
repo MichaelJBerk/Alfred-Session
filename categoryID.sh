27c401540cfb1845c6b4d22e786a0c19616c606f
Michael Berk <michaelberkdev@gmail.com>
2022-03-29 13:36:02 -0400

    Update to use Python 3

---
+++ categoryID.sh
@@ -0,0 +1,6 @@
+#!/usr/local/bin/python3
+
+import sys
+
+url = 'start?categoryId=' + sys.argv[1]
+sys.stdout.write(url)
---
+++ categoryName.sh
@@ -0,0 +1,6 @@
+#!/usr/local/bin/python3
+
+import sys
+
+url = 'start?categoryName=' + sys.argv[1]
+sys.stdout.write(url)
